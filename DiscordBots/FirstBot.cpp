30d38457ff5d774c623d71a67d784337fbc80066
DawidFr <dafr0@wp.pl>
2024-07-29 21:21:36 +0200

    init

---
+++ DiscordBots/FirstBot.cpp
@@ -0,0 +1,29 @@
+#include <dpp/dpp.h>
+
+using std::string, dpp::st_wait, dpp::cluster, ;
+
+const string BOT_TOKEN = "### ### ### ###"; //I do not share this one ;)
+
+int main() {
+    cluster bot(BOT_TOKEN);
+
+    bot.on_log(dpp::utility::cout_logger());
+
+    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
+        if (event.command.get_command_name() == "ping") {
+            event.reply("Pong!");
+        }
+    });
+
+    bot.on_ready([&bot](const dpp::ready_t& event) {
+        if (dpp::run_once<struct register_bot_commands>()) {
+            bot.global_command_create(
+                dpp::slashcommand("ping", "Ping pong!", bot.me.id)
+            );
+        }
+    });
+
+    bot.start(st_wait);
+
+    return 0;
+}
