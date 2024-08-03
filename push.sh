9ec64e22b9d972319a46cf6d0ec85e03963b4b1d
DawidFr <dafr0@wp.pl>
2024-08-04 01:12:29 +0200

    test

--- push.sh
+++ push.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+# Check if a commit message is provided
 if [ -z "$1" ]; then
-    echo -e "\033[31mNo commit message provided.\033[0m"
+    echo -e "\033[31m No commit message provided.\033[0m"
     exit 1
 fi
 
+# Add all changes to the staging area
 git add --all
+
+# Commit changes with the provided message
 git commit -m "$1"
 
-commit_url=$(git config --get remote.origin.url)
-commit_hash=$(git log -1 --pretty=format:"%H")
-commit_url="${commit_url/.git/}/commit/${commit_hash}"
+# Get the remote URL and format it for the commit link
+REMOTE_URL=$(git config --get remote.origin.url)
+COMMIT_HASH=$(git log -1 --pretty=format:"%H")
 
-echo "Commit URL: $commit_url"
+# Adjust URL for web-based repository (assuming GitHub, GitLab, etc.)
+COMMIT_URL=$(echo "$REMOTE_URL" | sed 's/\.git$/\/commit\/'"$COMMIT_HASH"'/')
+
+echo "Commit URL: $COMMIT_URL"
