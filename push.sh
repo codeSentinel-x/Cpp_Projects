9b609b4649638f2013dbf341a981519d15c8654e
DawidFr <dafr0@wp.pl>
2024-08-04 00:44:39 +0200

    test

--- push.sh
+++ push.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+    if [ -z "$1" ]; then
+        echo "\033[31;No commit message provided."
+        exit 1
+    fi
+
     git add --all
     git commit -m "$1"
-    git push -u origin master 
-
+    git push -u origin master #--force 
+    echo "https://github.com/codeSentinel-x/Slack-project/commit/$(git log -1 --pretty=format:"%H")"
 
