49653824bef90d3919722755eadc37951f9298fc
DawidFr <dafr0@wp.pl>
2024-08-04 01:11:41 +0200

    test

--- push.sh
+++ push.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo -e "\033[31m No commit message provided.\033[0m"
+    echo -e "\033[31mNo commit message provided.\033[0m"
     exit 1
 fi
 
 git add --all
 git commit -m "$1"
 
-
-echo "Commit URL:$(echo "$(git config --get remote.origin.url)" | sed 's/\.git$/\/commit\/'"$(git log -1 --pretty=format:"%H")"'/')"
+echo "Commit URL: $(git config --get remote.origin.url | sed 's/\.git$/\/commit\/'"$(git log -1 --pretty=format:"%H")"'/')"
